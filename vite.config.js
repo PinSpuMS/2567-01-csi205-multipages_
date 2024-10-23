74dc1fe8256e84e43f4112c4fb10e917a26ecac5
PinSpuMS <pin.ch@spulive.net>
2024-10-23 09:07:12 +0700

    delete source code

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/2567-01-csi205-multipages_/',
-})
